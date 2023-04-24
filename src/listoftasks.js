70e1e1c55e500deb057b233ecc82cea28728588a
priscilla <priscilla.lee.son@gmail.com>
2023-04-24 12:11:53 -0700

    Remove example tasks in listOfTasks

--- src/listoftasks.js
+++ src/listoftasks.js
@@ -1,5 +1,6 @@
 export const listOfTasks = [
-  {
+  /*these are examples of tasks that can be used in the list*/
+  /*{
     id: 0,
     task: "grocery shopping",
     completed: false,
@@ -8,7 +9,7 @@ export const listOfTasks = [
   { id: 2, task: "study", completed: false },
   { id: 3, task: "read a book", completed: false },
   { id: 4, task: "water the plants", completed: false },
-  { id: 5, task: "cook dinner", completed: false },
+  { id: 5, task: "cook dinner", completed: false }, */
 ];
 
 export const mappedTasks = listOfTasks.map((task) => {
