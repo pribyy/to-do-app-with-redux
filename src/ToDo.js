ac3fc2eeda504f3093ae545dfa505188d92f8482
priscilla <priscilla.lee.son@gmail.com>
2023-04-26 16:09:18 -0700

    Edit ToDo

--- src/ToDo.js
+++ src/ToDo.js
@@ -51,25 +51,26 @@ const AddItemsContainer = (props) => {
   };
 
   return (
-    <div className="relative">
+    <div className="relative ">
       <input
         type="text"
         placeholder="Buy apples..."
-        className="w-full p-2 mb-5 border rounded"
+        className="w-full pt-4 pb-3 px-4 text-lg mb-5 border rounded"
         //props.newListItem is state variable in parent
         value={props.newListItem}
         onKeyDown={handleKeyDown}
         onChange={(e) => props.setNewListItem(e.target.value)}
-      ></input>
+      />
       <submit
         //disable button if blank
         disabled={!props.newListItem}
+        tabIndex={0}
         onClick={handleSubmit}
         className={
           //if disabled, button is grey, otherwise, multi-color gradient
           !props.newListItem
-            ? `absolute bottom-4 h-10 px-3 py-2 m-2 border rounded bg-sky-100`
-            : `absolute bottom-4 h-10 px-3 py-2 m-2 border rounded bg-gradient-to-r from-emerald-200 from 10% via-sky-200 to-indigo-200 to-90% cursor-pointer`
+            ? `absolute left-100 px-6 py-4 ml-2 mb-5 border rounded bg-sky-100`
+            : `absolute px-6 py-4 ml-2 mb-5 border rounded bg-gradient-to-r from-emerald-200 from 10% via-sky-200 to-indigo-200 to-90% cursor-pointer`
         }
       >
         <div>+</div>
@@ -97,6 +98,14 @@ const ListContainer = (props) => {
     });*/
   };
 
+  const handleKeyDown = (e, task) => {
+    //keyCode 46 === Delete button
+    console.log(`I pressed delete`);
+    if (e.key === "Delete" && clickedTask?.id === task.id) {
+      handleDelete(task);
+    }
+  };
+
   const taskItemsRendered = props.toDoList.map((task, index) => {
     //index here is position within the rendered mapped list
     console.log(`this is task: ${task}, this is index: ${index}`, task);
@@ -105,11 +114,13 @@ const ListContainer = (props) => {
     return (
       <div
         key={task.id}
+        tabIndex={0}
+        onKeyDown={(e) => handleKeyDown(e, task)}
         onClick={() => handleHighlightClick(task)}
         className={
           isClickedTask
-            ? "hover:cursor-pointer hover:border-3 hover:border-blue-400 active:border-blue-400 flex justify-between py-2 px-4 mb-px w-50 h-300 border rounded bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-200 to-90%"
-            : "hover:cursor-pointer hover:border-3 hover:border-blue-400 active:border-blue-400 flex justify-between py-2 px-4 mb-px w-50 h-300 border rounded bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%"
+            ? "hover:cursor-pointer hover:border-3 hover:border-blue-400 active:border-blue-400 active:border-3 flex justify-between text-lg pt-4 pb-3 px-4 mb-px w-50 h-300 border rounded bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-200 to-90%"
+            : "hover:cursor-pointer hover:border-3 hover:border-blue-400 active:border-blue-400 flex justify-between pt-4 pb-3 text-lg px-4 mb-px w-50 h-300 border rounded bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%"
         }
       >
         <p>{task.task}</p>
