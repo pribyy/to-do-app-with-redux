7c852b4c578927520c27e0432f7c1b924e879b04
priscilla <priscilla.lee.son@gmail.com>
2023-05-12 22:43:20 -0700

    connect redux store to app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+//connecting store to our app
 import store from "./redux/store";
 import { Provider } from "react-redux";
+//
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/*connecting store via provider*/}
     <Provider store={store}>
       <App />
     </Provider>
