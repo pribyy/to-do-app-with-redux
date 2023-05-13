400e79e0f3e9dbfd3c9a3ef237e9114570560183
priscilla <priscilla.lee.son@gmail.com>
2023-05-12 22:44:32 -0700

    Add notes to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
+//configure store is a function that creates our store to hold all of our state
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 
+//this store holds many reducers
 export default configureStore({
   reducer: {
+    //give it a name: todos
     todos: todoReducer,
   },
 });
