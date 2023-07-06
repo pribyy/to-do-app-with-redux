6f857094a00f05d873f3c35f2223243507592139
priscilla <priscilla.lee.son@gmail.com>
2023-07-06 15:01:54 -0700

    Configure persist-store in redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,29 @@
 //configure store is a function that creates our store to hold all of our state
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
 import todoReducer from "./todoSlice";
 import inputValueReducer from "./inputValueSlice";
 import lightDarkThemeReducer from "./lightDarkThemeSlice";
 
-//this store holds many reducers
-export default configureStore({
-  reducer: {
-    inputValues: inputValueReducer,
-    todos: todoReducer,
-    theme: lightDarkThemeReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  inputValues: inputValueReducer,
+  todos: todoReducer,
+  theme: lightDarkThemeReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//this store holds many reducers
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
