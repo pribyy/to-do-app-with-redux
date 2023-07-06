91d3aa503c3f46b4fb5b971e0a09e32a0e7daf3a
priscilla <priscilla.lee.son@gmail.com>
2023-07-06 15:02:49 -0700

    Delete comment

--- src/redux/lightDarkThemeSlice.js
+++ src/redux/lightDarkThemeSlice.js
@@ -1,14 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// where and how do i use useeffect with redux?
-// //   useEffect(() => {
-//     //adds "dark" or "light" class to the body of the html document
-//     if (theme === "dark") {
-//         document.documentElement.classList.add("dark");
-//       } else {
-//         document.documentElement.classList.remove("dark");
-//       }
-
 const lightDarkThemeSlice = createSlice({
   name: "lightDarkTheme",
   initialState: "light",
