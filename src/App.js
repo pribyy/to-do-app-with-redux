9fda34aef674f114ed24ef5e7c959d23bd770161
priscilla <priscilla.lee.son@gmail.com>
2023-07-06 15:04:38 -0700

    Small edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import { ToDoContainer } from "./ToDoContainer";
-import { AddItemsInputandSubmitButton } from "./AddItemsInputandSubmitButton";
-import { ToDoList } from "./ToDoList";
-import { DarkandLightModeToggleButton } from "./DarkModeToggleButton";
+import { ToDoContainer } from "./components/ToDoContainer";
+import { AddItemsInputandSubmitButton } from "./components/AddItemsInputandSubmitButton";
+import { ToDoList } from "./components/ToDoList";
+import { DarkandLightModeToggleButton } from "./components/DarkModeToggleButton";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const theme = useSelector((state) => state.theme);
+  console.log(`this is the current mode: ${theme}`);
+  useEffect(() => {
+    //adds "dark" or "light" class to the body of the html document
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("light");
+    }
+  });
   return (
-    <div className="h-screen p-10">
+    <div
+      className={
+        theme === "light" ? "h-screen p-10" : "h-screen p-10 dark:bg-black"
+      }
+    >
       <DarkandLightModeToggleButton />
       <ToDoContainer>
         {/* AddItemsInput+SubmitButton is the input */}
